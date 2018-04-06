ff1449d094f75496ed7ce71c8caba592e00bede8
krazijames <krazijames@ridi.com>
2018-04-06 19:44:59 +0900

    Remove unused client code

--- client/src/menu.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import { Menu } from '@ridi/cms-ui';
-
-ReactDom.render(
-  <Menu items={window.menuData} />,
-  document.getElementById('menu'),
-);
