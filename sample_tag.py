cf4feeb62e0e23cdc5baf19c85cf7036ec1c2386
Jihun Kim <devgrapher@ridi.com>
2018-04-17 15:47:45 +0900

    Update comments for tag sample

--- sample_tag.py
+++ sample_tag.py
@@ -1,3 +1,11 @@
+''' Tag API example
+
+Make sure to run `make cms-up` before you test this example.
+
+The API offers read-only operations with the tags.
+Writing operations can only be made in CMS-Admin service(http://localhost/super/tags).
+'''
+
 from ridi.cms.config import Config
 from ridi.cms.cms_client import AdminUser
 from ridi.cms.cms_client import AdminMenu
@@ -8,6 +16,7 @@ config.RPC_URL = 'http://localhost'
 
 admin_user = AdminUser(config)
 admin_tag = AdminTag(config)
+admin_menu = AdminMenu(config)
 user = admin_user.getUser('admin')
 
 print('\n#Get all tags assigned to the user\n')
@@ -18,10 +27,12 @@ print('\n#Get tag objects from tag ids\n')
 tags = admin_tag.getAdminTags(tag_ids)
 print(tags)
 
-print('\n#Get menus under the tags\n')
-for tag_id in tag_ids:
-    menu_ids = admin_tag.getAdminTagMenus(tag_id)
-    print(menu_ids)
+print('\n#Get menu urls under the tag\n')
+if tag_ids:
+    menu_ids = admin_tag.getAdminTagMenus(tag_ids[0])
+    menus = admin_menu.getMenus(menu_ids)
+    urls = list(map(lambda m: m.menu_url, menus))
+    print(urls)
 
 print('\n#Get users who use the tag\n')
 user_ids = admin_tag.getAdminIdsFromTags(tag_ids)
